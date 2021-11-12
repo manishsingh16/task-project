a84704b69df7b5d2e1c8abfeeab71811d2e28dc4
manishsingh16 <manishsj97@gmail.com>
2021-11-12 19:47:03 +0530

    add first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('buy/{cookies}', function ($cookies) {
+    $wallet = Auth::user()->wallet;
+    Auth::user()->update(['wallet' => $wallet - $cookies * 1]);
+    Log:info('User ' . $user->email . ' have bought ' . $cookies . ' cookies'); // we need to log who ordered and how much
+    return 'Success, you have bought ' . $cookies . ' cookies!';
+});
